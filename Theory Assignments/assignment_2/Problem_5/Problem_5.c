78884e6c57c0271d4ae5c5818db119c8e5a46007
Zahran000 <zahranc04@gmail.com>
2024-11-16 15:43:05 +0500

    Update Problem_5.c

--- Theory Assignments/assignment_2/Problem_5/Problem_5.c
+++ Theory Assignments/assignment_2/Problem_5/Problem_5.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+int main(){
+    int n, arr[n],max=0;
+    printf("Enter size of array : ");
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++){
+        printf("Enter number %d : ", i+1);
+        scanf("%d", &arr[i]);
+        if(arr[i]>max) max=arr[i];
+    }
+
+    printf("\nHorizontal Histogram:\n");
+    for(int i=0; i<n; i++){
+        printf("Value %d: ", i+1);
+        for(int j=0; j<arr[i]; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    printf("Vertical Histogram:\n");
+    for(int i=0; i<max; i++){
+        for(int j=0; j<n; j++){
+            if(arr[j]>=max-i) printf("* ");
+            else printf("  ");
+        }
+        printf("\n");
+        if(i==max-1){
+            for(int i=0; i<n; i++){
+                printf("%d ",arr[i]);
+            }
+        }
+    }
+}
