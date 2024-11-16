6eb36c647097aceb4fa782b55bf998a0daf56085
Zahran000 <zahranc04@gmail.com>
2024-11-16 14:37:33 +0500

    Update Problem_6.c

--- Theory Assignments/assignment_2/Problem_6/Problem_6.c
+++ Theory Assignments/assignment_2/Problem_6/Problem_6.c
@@ -1,1 +1,30 @@
 
+#include<stdio.h>
+int match(int n)
+{
+    if (n%5==0) 
+	{
+        return -1;
+    }
+	else
+	{
+        return n%5;
+    }
+}
+int main()
+{ 
+    int n;
+	printf("Enter the number of matches: ");
+	scanf("%d", &n);
+	match(n);
+	int result=match(n);
+    if (result==-1) 
+	{
+        printf("Return -1");
+    }
+	else
+	{
+        printf("A should pick %d matchstick(s).\n", result);
+    }
+}
+
